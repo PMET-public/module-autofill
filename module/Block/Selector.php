3cce4ad10e87b6e074cce8fc1d0b2f12cc8dd12d
Matt Johnson <matt.johnson@classyllama.com>
2015-07-28 10:44:15 -0500

    Block - Code Cleanup and dependency injected helper class Admin - Adjustment to ACL sort order and making the persona password plain text instead of encrypted Helper - Code Cleanup Javascript - Code Cleanup and added mapping for sign in modal form on checkout page Layout - Code Cleanup CSS - Added z-index for applying autofill to popup modals, switched to LESS and compiled into entire site's styles-m.css instead of as a separate linked style sheet Template - Code Cleanup and  use of dependency injected helper

--- module/Block/Selector.php
+++ module/Block/Selector.php
@@ -2,112 +2,125 @@
 
 namespace MagentoEse\AutoFill\Block;
 
+use \Magento\Framework\View\Element\Template;
+
 /**
  * Auto Fill Selector block
  */
-class Selector extends \Magento\Framework\View\Element\Template
+class Selector extends Template
 {
 
-    /* Persona Enabled */
-    protected $_configPathPersonaEnabled = 'magentoese_autofill/persona_%s/enable';
+    // Persona Enabled
+    const XML_PATH_PERSONA_ENABLED = 'magentoese_autofill/persona_%s/enable';
 
-    /* Email and Password Values */
-    protected $_configPathEmail = 'magentoese_autofill/persona_%s/email_value';
-    protected $_configPathPassword = 'magentoese_autofill/persona_%s/password_value';
+    // Email and Password Values
+    const XML_PATH_EMAIL = 'magentoese_autofill/persona_%s/email_value';
+    const XML_PATH_PASSWORD = 'magentoese_autofill/persona_%s/password_value';
 
-    /* General Values */
-    protected $_configPathLabel = 'magentoese_autofill/persona_%s/label';
-    protected $_configPathFirstname = 'magentoese_autofill/persona_%s/firstname_value';
-    protected $_configPathLastname = 'magentoese_autofill/persona_%s/lastname_value';
-    protected $_configPathCompany = 'magentoese_autofill/persona_%s/company_value';
-    protected $_configPathAddress = 'magentoese_autofill/persona_%s/address_value';
-    protected $_configPathCountry = 'magentoese_autofill/persona_%s/country_value';
-    protected $_configPathCity = 'magentoese_autofill/persona_%s/city_value';
-    protected $_configPathState = 'magentoese_autofill/persona_%s/state_value';
-    protected $_configPathZip = 'magentoese_autofill/persona_%s/zip_value';
-    protected $_configPathTelephone = 'magentoese_autofill/persona_%s/telephone_value';
-    protected $_configPathFax = 'magentoese_autofill/persona_%s/fax_value';
+    // General Values
+    const XML_PATH_LABEL = 'magentoese_autofill/persona_%s/label';
+    const XML_PATH_FIRST_NAME = 'magentoese_autofill/persona_%s/firstname_value';
+    const XML_PATH_LAST_NAME = 'magentoese_autofill/persona_%s/lastname_value';
+    const XML_PATH_COMPANY = 'magentoese_autofill/persona_%s/company_value';
+    const XML_PATH_ADDRESS = 'magentoese_autofill/persona_%s/address_value';
+    const XML_PATH_COUNTRY = 'magentoese_autofill/persona_%s/country_value';
+    const XML_PATH_CITY = 'magentoese_autofill/persona_%s/city_value';
+    const XML_PATH_STATE = 'magentoese_autofill/persona_%s/state_value';
+    const XML_PATH_ZIP = 'magentoese_autofill/persona_%s/zip_value';
+    const XML_PATH_TELEPHONE = 'magentoese_autofill/persona_%s/telephone_value';
+    const XML_PATH_FAX = 'magentoese_autofill/persona_%s/fax_value';
 
-    /* CC Payment Values */
-    protected $_configPathCcType = 'magentoese_autofill/persona_%s/cc_type';
-    protected $_configPathCcNumber = 'magentoese_autofill/persona_%s/cc_number';
-    protected $_configPathCcMonth = 'magentoese_autofill/persona_%s/cc_month';
-    protected $_configPathCcYear = 'magentoese_autofill/persona_%s/cc_year';
-    protected $_configPathCcVerificationNumber = 'magentoese_autofill/persona_%s/cc_verification_number';
+    // CC Payment Values
+    const XML_PATH_CC_TYPE = 'magentoese_autofill/persona_%s/cc_type';
+    const XML_PATH_CC_NUMBER = 'magentoese_autofill/persona_%s/cc_number';
+    const XML_PATH_CC_MONTH = 'magentoese_autofill/persona_%s/cc_month';
+    const XML_PATH_CC_YEAR = 'magentoese_autofill/persona_%s/cc_year';
+    const XML_PATH_CC_VERIFICATION_NUMBER = 'magentoese_autofill/persona_%s/cc_verification_number';
 
-    /**
-     * The number of personas defined in the system configuration
-     */
-    protected $_personaCount = 7;
+    // The number of personas defined in the system configuration
+    const PERSONA_COUNT = 7;
 
     /**
      * Persona Data
      *
-     * @var
+     * @var []
      */
-    protected $_personas = array();
+    protected $personas = [];
 
     /**
-     * @param \Magento\Framework\View\Element\Template\Context $context
+     * Auto Fill Helper
+     *
+     * @var \MagentoEse\AutoFill\Helper\Data
+     */
+    public $helpper;
+
+    /**
+     * @param Template\Context $context
+     * @param \MagentoEse\AutoFill\Helper\Data $helper
      * @param array $data
      */
-    public function __construct(\Magento\Framework\View\Element\Template\Context $context, array $data = [])
-    {
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \MagentoEse\AutoFill\Helper\Data $helper,
+        array $data = []
+    ) {
+        $this->helpper = $helper;
         parent::__construct($context, $data);
     }
 
     /**
      * Retrieve loaded category collection
      *
-     * @return var
+     * @return array
      */
     public function getPersonas()
     {
-        if (count($this->_personas) === 0) {
+        if (count($this->personas) === 0) {
 
-            for ($i = 1; $i <= $this->_personaCount; $i++) {
-                if ($this->_scopeConfig->getValue(sprintf($this->_configPathPersonaEnabled, $i))) {
+            for ($i = 1; $i <= self::PERSONA_COUNT; $i++) {
+                if ($this->_scopeConfig->getValue(sprintf(self::XML_PATH_PERSONA_ENABLED, $i))) {
 
                     // Use label, if set, otherwise use full name
-                    $label = $this->_scopeConfig->getValue(sprintf($this->_configPathLabel, $i));
+                    $label = $this->_scopeConfig->getValue(sprintf(self::XML_PATH_LABEL, $i));
                     if (empty($label)) {
-                        $label = $this->_scopeConfig->getValue(sprintf($this->_configPathFirstname, $i))
+                        $label = $this->_scopeConfig->getValue(sprintf(self::XML_PATH_FIRST_NAME, $i))
                             . ' '
-                            . $this->_scopeConfig->getValue(sprintf($this->_configPathLastname, $i));
+                            . $this->_scopeConfig->getValue(sprintf(self::XML_PATH_LAST_NAME, $i));
                     }
 
                     // Build optionData from config values
-                    $optionData = array();
-                    $optionData['email'] = $this->_scopeConfig->getValue(sprintf($this->_configPathEmail, $i));
-                    $optionData['password'] = $this->_scopeConfig->getValue(sprintf($this->_configPathPassword, $i));
+                    $optionData = [
+                        'email' => $this->_scopeConfig->getValue(sprintf(self::XML_PATH_EMAIL, $i)),
+                        'password' => $this->_scopeConfig->getValue(sprintf(self::XML_PATH_PASSWORD, $i)),
 
-                    $optionData['label'] = $this->_scopeConfig->getValue(sprintf($this->_configPathLabel, $i));
-                    $optionData['firstname'] = $this->_scopeConfig->getValue(sprintf($this->_configPathFirstname, $i));
-                    $optionData['lastname'] = $this->_scopeConfig->getValue(sprintf($this->_configPathLastname, $i));
+                        'label' => $this->_scopeConfig->getValue(sprintf(self::XML_PATH_LABEL, $i)),
+                        'firstname' => $this->_scopeConfig->getValue(sprintf(self::XML_PATH_FIRST_NAME, $i)),
+                        'lastname' => $this->_scopeConfig->getValue(sprintf(self::XML_PATH_LAST_NAME, $i)),
 
-                    $optionData['company'] = $this->_scopeConfig->getValue(sprintf($this->_configPathCompany, $i));
-                    $optionData['address'] = $this->_scopeConfig->getValue(sprintf($this->_configPathAddress, $i));
-                    $optionData['country'] = $this->_scopeConfig->getValue(sprintf($this->_configPathCountry, $i));
-                    $optionData['city'] = $this->_scopeConfig->getValue(sprintf($this->_configPathCity, $i));
-                    $optionData['state'] = $this->_scopeConfig->getValue(sprintf($this->_configPathState, $i));
-                    $optionData['zip'] = $this->_scopeConfig->getValue(sprintf($this->_configPathZip, $i));
-                    $optionData['telephone'] = $this->_scopeConfig->getValue(sprintf($this->_configPathTelephone, $i));
-                    $optionData['fax'] = $this->_scopeConfig->getValue(sprintf($this->_configPathFax, $i));
+                        'company' => $this->_scopeConfig->getValue(sprintf(self::XML_PATH_COMPANY, $i)),
+                        'address' => $this->_scopeConfig->getValue(sprintf(self::XML_PATH_ADDRESS, $i)),
+                        'country' => $this->_scopeConfig->getValue(sprintf(self::XML_PATH_COUNTRY, $i)),
+                        'city' => $this->_scopeConfig->getValue(sprintf(self::XML_PATH_CITY, $i)),
+                        'state' => $this->_scopeConfig->getValue(sprintf(self::XML_PATH_STATE, $i)),
+                        'zip' => $this->_scopeConfig->getValue(sprintf(self::XML_PATH_ZIP, $i)),
+                        'telephone' => $this->_scopeConfig->getValue(sprintf(self::XML_PATH_TELEPHONE, $i)),
+                        'fax' => $this->_scopeConfig->getValue(sprintf(self::XML_PATH_FAX, $i)),
 
-                    $optionData['cc_type'] = $this->_scopeConfig->getValue(sprintf($this->_configPathCcType, $i));
-                    $optionData['cc_number'] = $this->_scopeConfig->getValue(sprintf($this->_configPathCcNumber, $i));
-                    $optionData['cc_month'] = $this->_scopeConfig->getValue(sprintf($this->_configPathCcMonth, $i));
-                    $optionData['cc_year'] = $this->_scopeConfig->getValue(sprintf($this->_configPathCcYear, $i));
-                    $optionData['cc_verification_number'] = $this->_scopeConfig->getValue(sprintf($this->_configPathCcVerificationNumber, $i));
+                        'cc_type' => $this->_scopeConfig->getValue(sprintf(self::XML_PATH_CC_TYPE, $i)),
+                        'cc_number' => $this->_scopeConfig->getValue(sprintf(self::XML_PATH_CC_NUMBER, $i)),
+                        'cc_month' => $this->_scopeConfig->getValue(sprintf(self::XML_PATH_CC_MONTH, $i)),
+                        'cc_year' => $this->_scopeConfig->getValue(sprintf(self::XML_PATH_CC_YEAR, $i)),
+                        'cc_verification_number' => $this->_scopeConfig->getValue(sprintf(self::XML_PATH_CC_VERIFICATION_NUMBER, $i)),
+                    ];
 
                     // Set the persona data for this persona
-                    $this->_personas[$i]['optionLabel'] = $label;
-                    $this->_personas[$i]['optionValue'] = $i;
-                    $this->_personas[$i]['optionData'] = json_encode($optionData);
+                    $this->personas[$i]['optionLabel'] = $label;
+                    $this->personas[$i]['optionValue'] = $i;
+                    $this->personas[$i]['optionData'] = json_encode($optionData);
                 }
             }
         }
 
-        return $this->_personas;
+        return $this->personas;
     }
 }
